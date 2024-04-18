874aac8241677c7001a9163fcf035ba674b29aff
Swayam <166048058+Swaysigh@users.noreply.github.com>
2024-04-18 22:21:32 +0530

    Update typecasting.py

--- Basics/typecasting.py
+++ Basics/typecasting.py
@@ -1,11 +1,11 @@
 a = "5"
 b = "2"
-print( a + b )
-      # explicit type casting
-a = "28"
-b = "19"
-print( int(a) + int(b) )
-      # implicit typecasting
-a = 55.5
-b = 68
-print( a + b )
+print( a + b )   #adding both strings
+      #explicit typecasting
+c = "28"
+d = "19"
+print( int(c) + int(d) )
+      #implicit typecasting
+x = 55.5
+y = 68
+print( x + y )

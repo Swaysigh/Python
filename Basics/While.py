4472e97b2b3dd6a4a5e39cf22d41ab927ce3be59
Swayam <166048058+Swaysigh@users.noreply.github.com>
2024-05-18 21:00:40 +0530

    Create While.py

---
+++ Basics/While.py
@@ -0,0 +1,20 @@
+i = int(input("Enter The number: "))
+while( i <= 38):
+    i = int(input("Enter The number: "))
+    print(i)
+    i = i + 1
+
+# Decrementing Loop
+count = 5
+while (count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("Out of the loop")
+
+# Do-While Loop
+while True:
+  x = int(input("Enter the number :"))
+          print(x)
+  if not x>0:
+      break

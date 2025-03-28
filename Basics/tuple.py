a5dd4625ccb2b99c5b907a9c63beba87a0c74fa9
Swayam <assentioncreed3@gmail.com>
2025-03-28 19:55:50 +0530

    changes

--- Basics/tuple.py
+++ Basics/tuple.py
@@ -36,3 +36,13 @@ print('Count of 3 in tuple1 is: ',ray)
 # Length of Tuple
 len = len(tuple1)
 print(len)
+
+
+
+tuple1 = (0, 1, 2, 3, 2, 31, 1, 3, 2, 3)
+res = tuple1.count(3)
+res = tuple1.index(3)
+res = tuple1.index(31)
+res = tuple1.index(3, 4, 8)
+res = len(tuple1)
+print('Count of 3 in tuple1 is:', res)
